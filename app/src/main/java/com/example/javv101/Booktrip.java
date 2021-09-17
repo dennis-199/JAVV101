fe88b9147e6accb7618357f261e67e564d41d06d
dennis-199 <otieno.dennis@strathmore.edu>
2021-09-17 19:44:28 +0300

    Updated user interface

--- app/src/main/java/com/example/javv101/Booktrip.java
+++ app/src/main/java/com/example/javv101/Booktrip.java
@@ -3,12 +3,105 @@ package com.example.javv101;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Booktrip extends AppCompatActivity {
+    ArrayList<String> destinations=new ArrayList<>();
+    ArrayList<String> pickuppoints=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booktrip);
+
+        //SPINNER
+        Spinner sp= (Spinner) findViewById(R.id.spinner);
+        Spinner sp2=(Spinner) findViewById(R.id.spinner2);
+
+        //FILL DATA
+        fillData();
+
+        //ADAPTR
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,destinations);
+        sp.setAdapter(adapter);
+        ArrayAdapter<String> adapter1=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,pickuppoints);
+        sp2.setAdapter(adapter1);
+
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(Booktrip.this, destinations.get(i), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        sp2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(Booktrip.this, pickuppoints.get(i), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    //FILL DATA
+    private void fillData()
+    {
+        destinations.clear();
+        pickuppoints.clear();
+
+        //FILL
+        destinations.add("Uthiru");
+        destinations.add("Kikuyu");
+        destinations.add("CBD");
+        destinations.add("West Madaraka");
+        destinations.add("Westlands");
+        destinations.add("Umoja");
+        destinations.add("South B");
+        destinations.add("Lang'ata");
+        destinations.add("Kileleshwa");
+        destinations.add("Ngong");
+        destinations.add("Juja");
+        destinations.add("Ruiru");
+        destinations.add("Rongo");
+        destinations.add("Oyugis");
+        destinations.add("Kisumu");
+        destinations.add("Nyeri");
+        destinations.add("Machakos");
+        destinations.add("Kitui");
+
+        pickuppoints.add("Uthiru");
+        pickuppoints.add("Kikuyu");
+        pickuppoints.add("CBD");
+        pickuppoints.add("West Madaraka");
+        pickuppoints.add("Westlands");
+        pickuppoints.add("Umoja");
+        pickuppoints.add("South B");
+        pickuppoints.add("Lang'ata");
+        pickuppoints.add("Kileleshwa");
+        pickuppoints.add("Ngong");
+        pickuppoints.add("Juja");
+        pickuppoints.add("Ruiru");
+        pickuppoints.add("Rongo");
+        pickuppoints.add("Oyugis");
+        pickuppoints.add("Kisumu");
+        pickuppoints.add("Nyeri");
+        pickuppoints.add("Machakos");
+        pickuppoints.add("Kitui");
+
     }
 }
--- app/src/main/java/com/example/javv101/Pay.java
+++ app/src/main/java/com/example/javv101/Pay.java
@@ -3,12 +3,101 @@ package com.example.javv101;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Pay extends AppCompatActivity {
+    ArrayList<String> saccosname=new ArrayList<>();
+    ArrayList<String> vehiclesno=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
+
+        //SPINNER
+        Spinner sp= (Spinner) findViewById(R.id.spinner3);
+        Spinner sp2=(Spinner) findViewById(R.id.spinner4);
+
+        //FILL DATA
+        fillData();
+
+        //ADAPTR
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,saccosname);
+        sp.setAdapter(adapter);
+        ArrayAdapter<String> adapter1=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,vehiclesno);
+        sp2.setAdapter(adapter1);
+
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(Pay.this, saccosname.get(i), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        sp2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(Pay.this, vehiclesno.get(i), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    //FILL DATA
+    private void fillData()
+    {
+        saccosname.clear();
+        vehiclesno.clear();
+
+        //FILL
+        saccosname.add("Highrise");
+        saccosname.add("Buruburu");
+        saccosname.add("Kinatwa");
+        saccosname.add("Agape");
+        saccosname.add("2 NK");
+        saccosname.add("St.Mary's");
+        saccosname.add("South B Travellers");
+        saccosname.add("Embassavo");
+        saccosname.add("Kileleshwa");
+        saccosname.add("city hoppa");
+        saccosname.add("city shuttle");
+        saccosname.add("Ruiru");
+
+
+        vehiclesno.add("A1");
+        vehiclesno.add("A2");
+        vehiclesno.add("A3");
+        vehiclesno.add("A4");
+        vehiclesno.add("A5");
+        vehiclesno.add("A6");
+        vehiclesno.add("A7");
+        vehiclesno.add("A9");
+        vehiclesno.add("A10");
+        vehiclesno.add("B1");
+        vehiclesno.add("B2");
+        vehiclesno.add("B3");
+        vehiclesno.add("B4");
+        vehiclesno.add("B5");
+        vehiclesno.add("B6");
+        vehiclesno.add("B7");
+        vehiclesno.add("B8");
+        vehiclesno.add("B9");
+        vehiclesno.add("B10");
+
     }
 }
